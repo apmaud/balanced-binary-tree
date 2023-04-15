d3688f6d48fdf3f1f4b095965e3d139aeb66f211
Arjun Maudhoo <apm526@nyu.edu>
2023-04-15 17:07:56 -0400

    Add find function

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// const testArray = [1,4,2,3,5,6,7,6,7,1,2,5,7,8,9,10,23,54,32];
+// const cleanedTestArray = cleanArray(testArray);
+// const testArrayLength = cleanedTestArray.length;
+// console.log(cleanedTestArray);
+
 class Node {
     constructor(data) {
         this.data = data;
@@ -12,14 +17,18 @@ class Tree {
     }
 }
 
-function buildTree(array){
-
+function buildTree(array, start, end){
+    if (start > end){return null}
+    var midArray = parseInt((start+end)/2)
+    var node = new Node(array[midArray])
+    node.left = buildTree(array, start, (midArray-1))
+    node.right = buildTree(array, (midArray+1), end)
+    return node
 }
 
 function cleanArray(array){
-    return [...new Set(array)].sort();
+    return [...new Set(array)].sort((a,b) => a - b);
 }
-
 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
   if (node === null) {
@@ -32,4 +41,28 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
   if (node.left !== null) {
     prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
   }
+}
+
+function render(){
+    prettyPrint(buildTree(cleanedTestArray, 0, testArrayLength-1));
+}
+
+function find(node, num){
+    if (node == null || node.data == num){
+        return node
+    }
+    else if (node.data < num){
+        return find(node.right, num);
+    }
+    else {
+        return find(node.left, num);
+    }
+}
+
+function ins(num){
+
+}
+
+function del(num){
+
 }
