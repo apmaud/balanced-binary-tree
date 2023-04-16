2814a010b07c85770e1fd43aacca1d1d1d7cf789
Arjun Maudhoo <apm526@nyu.edu>
2023-04-16 16:27:51 -0400

    Add ins and del functions

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-// const testArray = [1,4,2,3,5,6,7,6,7,1,2,5,7,8,9,10,23,54,32];
-// const cleanedTestArray = cleanArray(testArray);
-// const testArrayLength = cleanedTestArray.length;
-// console.log(cleanedTestArray);
+const testArray = [1,4,2,3,5,6,7,6,7,1,2,5,7,8,9,10,23,54,32];
+const cleanedTestArray = cleanArray(testArray);
+const testArrayLength = cleanedTestArray.length;
+console.log(cleanedTestArray);
 
 class Node {
     constructor(data) {
@@ -12,57 +12,84 @@ class Node {
 }
 
 class Tree {
-    constructor(array){
-        this.root = buildTree(array);
+    constructor(){
+        this.root = null
     }
-}
 
-function buildTree(array, start, end){
-    if (start > end){return null}
-    var midArray = parseInt((start+end)/2)
-    var node = new Node(array[midArray])
-    node.left = buildTree(array, start, (midArray-1))
-    node.right = buildTree(array, (midArray+1), end)
-    return node
-}
-
-function cleanArray(array){
-    return [...new Set(array)].sort((a,b) => a - b);
-}
-
-const prettyPrint = (node, prefix = '', isLeft = true) => {
-  if (node === null) {
-     return;
-  }
-  if (node.right !== null) {
-    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-  }
-  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
-  if (node.left !== null) {
-    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-  }
-}
-
-function render(){
-    prettyPrint(buildTree(cleanedTestArray, 0, testArrayLength-1));
-}
-
-function find(node, num){
-    if (node == null || node.data == num){
+    buildTree(array, start, end){
+        if (start > end){return null}
+        var midArray = parseInt((start+end)/2)
+        var node = new Node(array[midArray])
+        node.left = buildTree(array, start, (midArray-1))
+        node.right = buildTree(array, (midArray+1), end)
         return node
     }
-    else if (node.data < num){
-        return find(node.right, num);
+    
+    cleanArray(array){
+        return [...new Set(array)].sort((a,b) => a - b);
     }
-    else {
-        return find(node.left, num);
+    
+    render(){
+        prettyPrint(buildTree(cleanedTestArray, 0, testArrayLength-1));
+    }
+    
+    find(node, num){
+        if (node == null || node.data == num){
+            return node
+        }
+        else if (node.data < num){
+            return find(node.right, num);
+        }
+        else {
+            return find(node.left, num);
+        }
+    }
+    
+    ins(num){
+        if (!this.root){
+            this.root = new Node(num)
+        }
+        else {
+            this.insNode(this.root, num)
+        }
+    }
+
+    insNode(node, num){
+        if (node == null){
+            node = new Node(num);
+            return node
+        }
+        else if (node.data < num){
+            return insNode(node.right, num);
+        }
+        else {
+            return insNode(node.left, num);
+        }
+    }
+    
+    del(num){
+        if (!this.root){
+            return
+        }
+        else {
+            this.delNode(this.delNode(this.root, num))
+        }
+    }
+    
+    delNode(node, num){
+
     }
 }
 
-function ins(num){
-
-}
-
-function del(num){
-
+prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node === null) {
+       return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
 }
